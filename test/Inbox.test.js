17607c89231b6c02f2f8b793d501f4692fd619f4
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-24 00:02:17 +0800

    feat: remove Car class and remote it's test, add test for deploying contract and log fetched accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,28 +4,15 @@ const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return "stopped";
-  }
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
 
-  drive() {
-    return "vroom";
-  }
-}
-
-let car;
-
-beforeEach(async () => {
-  car = new Car();
+  //  Use one of those accoounts to deploy the contract
 });
 
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
-  });
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
 });
