c15be55de5a0d011729ba64729937b8656fe513f
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-24 19:07:14 +0800

    feat: Add tests for default and updated contract message
        - Verify contract deployment with a valid address
        - Check for default message upon deployment
        - Test message update functionality

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,18 +1,34 @@
-const assert = require("assert");
 const ganache = require("ganache");
+const assert = require("assert");
 const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
+const { contractInterface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+const INITIAL_MESSAGE = "Hi there!";
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   //  Use one of those accoounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(contractInterface))
+    .deploy({
+      data: bytecode,
+      arguments: [INITIAL_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(accounts);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
   });
 });
